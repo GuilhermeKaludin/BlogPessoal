9528e32ed4983f4b3b57f266cce7d2468d64d099
Guilherme Kaludin <guikaludin@outlook.com>
2025-01-17 11:45:50 -0300

    Criando Usuario e UsuarioLogin na model

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -15,60 +15,64 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-@Entity // indicar que a classe é uma tabela
-@Table(name = "tb_postagens") // indica o nome da tabela no banco de dados
+@Entity
+@Table(name = "tb_postagens")
 public class Postagem {
-
-	@Id // indicando que é a chave primaria
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
-
-	@NotBlank(message = "Esse campo é obrigatório") // not null
-	@Size(min = 5, max = 100, message = "Digite no minimo 5 caracteres e no maximo 100 caracteres")
+	
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String titulo;
-
-	@NotBlank(message = "Esse campo é obrigatório") // not null
-	@Size(min = 10, max = 1000, message = "Digite no minimo 10 caracteres e no maximo 1000 caracteres")
+	
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
 	private String texto;
-
+	
 	@UpdateTimestamp
 	private LocalDateTime data;
-
+	
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getTitulo() {
-		return titulo;
-	}
+    public String getTitulo() {
+        return this.titulo;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public String getTexto() {
-		return texto;
-	}
+    public String getTexto() {
+        return this.texto;
+    }
 
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 
-	public LocalDateTime getData() {
-		return data;
-	}
+    public LocalDateTime getData() {
+        return this.data;
+    }
 
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 
 	public Tema getTema() {
 		return tema;
@@ -78,4 +82,12 @@ public class Postagem {
 		this.tema = tema;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+    
 }
